acd39f66aeabd8c62eb62c9f08ff30e5650e756e
Artem Markin <rizeman01@gmail.com>
2024-09-30 15:00:15 +0300

    Add test description to tag.test.ts

--- __tests__/tag.test.ts
+++ __tests__/tag.test.ts
@@ -1,39 +1,41 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import Tag from '../src/classes/tag';
 
-test('br', () => {
-  const tag = new Tag('br');
-
-  expect(tag.toString()).toBe('<br>');
-});
-
-test('img', () => {
-  const tag = new Tag('img', { src: 'path/to/image' });
-
-  expect(tag.toString()).toBe('<img src="path/to/image">');
-});
-
-test('input', () => {
-  const tag = new Tag('input', { type: 'submit', value: 'Save' });
-
-  expect(tag.toString()).toBe('<input type="submit" value="Save">');
-});
-
-test('label', () => {
-  const tag = new Tag('label', {}, 'Email');
-
-  expect(tag.toString()).toBe('<label>Email</label>');
-});
-
-test('label with text', () => {
-  const tag = new Tag('label', { for: 'email' }, 'Email');
-
-  expect(tag.toString()).toBe('<label for="email">Email</label>');
-});
-
-test('div', () => {
-  const tag = new Tag('div');
-
-  expect(tag.toString()).toBe('<div></div>');
-});
+describe('Tag result text is correct for tag', function () {
+  test('br', () => {
+    const tag = new Tag('br');
+  
+    expect(tag.toString()).toBe('<br>');
+  });
+  
+  test('img', () => {
+    const tag = new Tag('img', { src: 'path/to/image' });
+  
+    expect(tag.toString()).toBe('<img src="path/to/image">');
+  });
+  
+  test('input', () => {
+    const tag = new Tag('input', { type: 'submit', value: 'Save' });
+  
+    expect(tag.toString()).toBe('<input type="submit" value="Save">');
+  });
+  
+  test('label', () => {
+    const tag = new Tag('label', {}, 'Email');
+  
+    expect(tag.toString()).toBe('<label>Email</label>');
+  });
+  
+  test('label with text', () => {
+    const tag = new Tag('label', { for: 'email' }, 'Email');
+  
+    expect(tag.toString()).toBe('<label for="email">Email</label>');
+  });
+  
+  test('div', () => {
+    const tag = new Tag('div');
+  
+    expect(tag.toString()).toBe('<div></div>');
+  });
+})
